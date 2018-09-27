d85a94416629c303ad86c029e9850aa8aab3bca3
fabianna <43337430+fabianna@users.noreply.github.com>
2018-09-27 17:02:35 +0200

    Add files via upload

---
+++ fabianna/filtrarDatos_SouthEastAsia.js
@@ -0,0 +1,21 @@
+function filtrarDatos_SouthEastAsia() {
+    datos = document.getElementsByTagName('table')[0].getElementsByTagName('tr')
+    str='[';
+    for (i=1;i<datos.length;i++) {
+	
+	descr = datos[i].children[0].textContent;
+	region = datos[i].children[3].textContent;
+	
+		if (descr === "Number of neonatal deaths (thousands)" && region === "South-East Asia")  {
+		  
+		  year  = datos[i].children[2].textContent;
+		  value = datos[i].children[4].textContent;
+		  str += "[" + year
+		  str += "," + value + "],"
+		  str += "\n"
+		}
+	}
+    str += "]";
+    return str;
+	
+}
