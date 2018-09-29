e1cde174d69d32f4d0341529e8220b7618a837ce
fabianna <43337430+fabianna@users.noreply.github.com>
2018-09-29 11:52:48 +0200

    Update leerTop5Cities.js

--- fabianna/leerTop5Cities.js
+++ fabianna/leerTop5Cities.js
@@ -5,12 +5,12 @@ function leerTop5Cities() {
     for (i=1;i<6;i++) {
 		  
 	city  = datos[i].children[1].textContent;
-	year1990 = datos[i].children[7].textContent.replace(',','');
-	year2000 = datos[i].children[6].textContent.replace(',','');
-	year2005 = datos[i].children[5].textContent.replace(',','');
-	year2010 = datos[i].children[4].textContent.replace(',','');
-	year2013 = datos[i].children[3].textContent.replace(',','');
-	year2014 = datos[i].children[2].textContent.replace(',','');
+	year1990 = datos[i].children[7].textContent.replace(',','').replace(',','');
+	year2000 = datos[i].children[6].textContent.replace(',','').replace(',','');
+	year2005 = datos[i].children[5].textContent.replace(',','').replace(',','');
+	year2010 = datos[i].children[4].textContent.replace(',','').replace(',','');
+	year2013 = datos[i].children[3].textContent.replace(',','').replace(',','');
+	year2014 = datos[i].children[2].textContent.replace(',','').replace(',','');
       	
 	str += "['" + city + "'"
 	str += "," + year1990 + "," + year2000 + "," + year2005 + "," + year2010 + "," + year2013 + "," + year2014 + "],"
