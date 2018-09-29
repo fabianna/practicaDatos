44d504b85e43965985b7ef1ff72ac95d0390f852
fabianna <43337430+fabianna@users.noreply.github.com>
2018-09-29 12:17:36 +0200

    Update leerTop5Cities.js

--- fabianna/leerTop5Cities.js
+++ fabianna/leerTop5Cities.js
@@ -1,16 +1,18 @@
 function leerTop5Cities() {
     datos = document.getElementsByTagName('table')[0].getElementsByTagName('tr')
     str = '[';
-    
+    str += "['City','1990','2000', '2005', '2010', '2013', '2014']";
+    str += "\n"	
+		
     for (i=1;i<6;i++) {
 		  
 	city  = datos[i].children[1].textContent;
-	year1990 = datos[i].children[7].textContent.replace(',','').replace(',','');
-	year2000 = datos[i].children[6].textContent.replace(',','').replace(',','');
-	year2005 = datos[i].children[5].textContent.replace(',','').replace(',','');
-	year2010 = datos[i].children[4].textContent.replace(',','').replace(',','');
-	year2013 = datos[i].children[3].textContent.replace(',','').replace(',','');
-	year2014 = datos[i].children[2].textContent.replace(',','').replace(',','');
+	y1 = datos[i].children[7].textContent.replace(',','').replace(',','');
+	y2 = datos[i].children[6].textContent.replace(',','').replace(',','');
+	y3 = datos[i].children[5].textContent.replace(',','').replace(',','');
+	y4 = datos[i].children[4].textContent.replace(',','').replace(',','');
+	y5 = datos[i].children[3].textContent.replace(',','').replace(',','');
+	y6 = datos[i].children[2].textContent.replace(',','').replace(',','');
       	
 	str += "['" + city + "'"
 	str += "," + year1990 + "," + year2000 + "," + year2005 + "," + year2010 + "," + year2013 + "," + year2014 + "],"
